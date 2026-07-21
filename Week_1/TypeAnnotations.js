2ac16d544d0e049d1691c464528544dee6b3251c
Hansika-Kommina <komminahansika@gmail.com>
2026-07-21 10:02:43 +0530

    Completed Week 1 to Week 4 TypeScript experiments

---
+++ Week_1/TypeAnnotations.js
@@ -0,0 +1,24 @@
+"use strict";
+// PROGRAM 1 : Variable Type Annotation
+console.log("Program 1 ");
+let name = "Hansika";
+let age = 20;
+let passed = true;
+console.log(name);
+console.log(age);
+console.log(passed);
+// PROGRAM 2 : Function Annotation
+console.log("\nProgram 2 ");
+function add(a, b) {
+    return a + b;
+}
+console.log(add(20, 30));
+// PROGRAM 3 : Array & Object Annotation
+console.log("\n Program 3 ");
+let marks = [90, 95, 98];
+let student = {
+    name: "Hansika",
+    roll: 101
+};
+console.log(marks);
+console.log(student);
